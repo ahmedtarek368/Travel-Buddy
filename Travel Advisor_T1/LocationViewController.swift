ef9ace72925a98515887ce79b53ce7b76b2925f5
ahmedtarek368 <ahmedtarek368@hotmail.com>
2020-03-18 12:51:55 -0700

    Edit : user location added

--- Travel Advisor_T1/LocationViewController.swift
+++ Travel Advisor_T1/LocationViewController.swift
@@ -8,45 +8,74 @@
 
 import UIKit
 import MapKit
-class LocationViewController: UIViewController {
-    @IBAction func segment(_ sender: UISegmentedControl) {
+class LocationViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
+
+    @IBOutlet weak var userLocationBtn: UIButton!
+    @IBOutlet weak var mymap: MKMapView!
+    
+    var viewOnUserLocation = false
+    let locationManager = CLLocationManager()
+    let destination = CLLocation(latitude:28.5376138,longitude:34.5155859)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centerViewOnDestinationLocation()
+        
+        let PointAnnotion = MKPointAnnotation()
+        PointAnnotion.coordinate = destination.coordinate
+        PointAnnotion.title="Daniela Village"
+        PointAnnotion.subtitle="3-star hotel"
+        mymap.addAnnotation(PointAnnotion)
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    
+    @IBAction func userLocationBtnAction(_ sender: Any) {
+        if viewOnUserLocation == false{         //View User Location
+            configureLocationServices()
+            userLocationBtn.setImage(UIImage(named: "LocationOn.png"), for: .normal)
+            viewOnUserLocation = true
+        }else if viewOnUserLocation == true{        //View Destination Location
+            centerViewOnDestinationLocation()
+            locationManager.stopUpdatingLocation()
+            userLocationBtn.setImage(UIImage(named: "LocationOff.png"), for: .normal)
+            viewOnUserLocation = false
+        }
+    }
+    
+    func configureLocationServices(){
+        let status = CLLocationManager.authorizationStatus()
+        if status == .notDetermined{
+            locationManager.requestAlwaysAuthorization()
+        }else if status == .authorizedAlways || status == .authorizedWhenInUse{
+            mymap.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    func centerViewOnDestinationLocation(){
+        let region = MKCoordinateRegion(center:destination.coordinate,latitudinalMeters:500,longitudinalMeters:500)
+        mymap.setRegion(region, animated: true)
+    }
+    func centerViewOnUserLocation(){
+        if let location = locationManager.location?.coordinate{
+            let Region = MKCoordinateRegion.init(center: location, latitudinalMeters: 500, longitudinalMeters: 500)
+            mymap.setRegion(Region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        let locationCoordinates = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let Region = MKCoordinateRegion.init(center: locationCoordinates, latitudinalMeters: 500, longitudinalMeters: 500)
+        mymap.setRegion(Region, animated: true)
+    }
+    
+    @IBAction func visionStyleSelection(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex==0{
             mymap.mapType = .standard
         }else{ mymap.mapType = .satellite}
     }
-    @IBOutlet weak var mymap: MKMapView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let applehq = CLLocation(latitude:37.334722,longitude:-122.008889)
-        let regionRadius:CLLocationDistance=1000.0
-        let region = MKCoordinateRegion(center:applehq.coordinate,latitudinalMeters:regionRadius,longitudinalMeters:regionRadius)
-        mymap.setRegion(region, animated: true)
-        let PointAnnotion = MKPointAnnotation()
-        PointAnnotion.coordinate = applehq.coordinate
-        PointAnnotion.title="Apple HQ"
-        PointAnnotion.subtitle="here"
-        mymap.addAnnotation(PointAnnotion)
-
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-extension LocationViewController:MKMapViewDelegate{
-    func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
-        print("rendring")
-    }
-    
-    
-    
 }
